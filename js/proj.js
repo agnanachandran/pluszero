6c66883cb6ad5be7b750c1200b2859210f324e2f
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2013-01-10 15:17:49 -0500

    added heading, changed basic appearances

--- js/proj.js
+++ js/proj.js
@@ -12,13 +12,22 @@
 //holo color:#00ddff
 
 $(document).ready(function() {
-	$('.mainpagetext').fadeIn('slow');
+	$('.projtext').fadeIn('slow');
+	$('#head').fadeIn('slow');
 	$('#endofpage').fadeIn('slow');
-	$('#plusZero').fadeIn('slow');
+
 	$(".glow").hover(function(){
 		$(this).animate({color:"#fff"},200);
 
 	}, function(){
 		$(this).animate({color:"#00ddff"}, 200);
 	});
+
+	$(".link").hover(function(){
+		$(this).animate({color:"#fff"},200);
+
+	}, function(){
+		$(this).animate({color:"#eee"}, 200);
+	});
+
 });
