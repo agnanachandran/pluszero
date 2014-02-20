a42de8d6d63d50dcf82ff711cbe4087f60cbf341
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-02-19 23:45:25 -0500

    Add some more images and simplify home page

--- source/ruby-on-everything/js/main.js
+++ source/ruby-on-everything/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     $("img.lazy").lazyload({
-        effect : "fadeIn"
+        effect: "fadeIn",
+        threshold : 200
+    });
+    $('.appear').hide();
+    // call waypoint plugin
+    $('.appear').waypoint(function(event, direction) {
+        // do your fade in here
+        $(this).fadeIn();
+    }, {
+    offset: function() {
+        // The bottom of the element is in view
+        return $.waypoints('viewportHeight') - $(this).outerHeight();
+        }
     });
 });
