d1b19b0fef4e9dc70de622979cfbfc86476bdb80
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-02-28 22:17:17 -0500

    Refactor. Update layouts.

--- source/js/orig/plusmain.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#plus-zero').fadeIn('slow');
-    $('#play-pong').click(function() {
-        $('#pong').animate({'opacity': '1'}, 'slow');
-        playPong();
-    });
-});
