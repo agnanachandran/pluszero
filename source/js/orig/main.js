25a8c68f921f29d3c6fa2230dfbc930631c40880
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-04-18 17:37:30 -0400

    Add pong to projects page.

--- source/js/orig/main.js
+++ source/js/orig/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $('#play-pong').click(function() {
         $('#pong').animate({'opacity': '1'}, 'slow');
-        playPong();
+        if (!!window.HTMLCanvasElement) {
+            playPong();
+        }
     });
 });
