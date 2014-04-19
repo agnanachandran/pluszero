0997d5d37810703711fc321ce91ef9d601c94746
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-04-19 15:15:23 -0400

    Prevent multiple pongs.

--- source/js/orig/main.js
+++ source/js/orig/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+    var clicked = false;
     $('#play-pong').click(function() {
-        $('#pong').animate({'opacity': '1'}, 'slow');
-        if (!!window.HTMLCanvasElement) {
-            playPong();
+        if (!clicked) {
+            $('#pong').animate({'opacity': '1'}, 'slow');
+            if (!!window.HTMLCanvasElement) {
+                playPong();
+            }
+            clicked = true;
+        } else {
+            window.location = document.getElementById('projects-link').href;
         }
     });
 });
